6aa87a9e5805411b053ba0b5a69d7e4c37508901
jsamol <julia.samol@gmail.com>
2018-06-03 18:18:14 +0200

    Add MainPage component

---
+++ react/lingua/src/components/mainpage/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from '../shared/Header';
+
+class MainPage extends Component {
+    render() {
+        return (
+            <Header/>
+        );
+    }
+}
+
+export default MainPage;
