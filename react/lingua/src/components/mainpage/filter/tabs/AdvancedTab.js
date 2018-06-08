20816c2a8427ce94fea6336d109e6a1fb15ea059
jsamol <julia.samol@gmail.com>
2018-06-08 11:39:35 +0200

    Add the advanced filtering form

--- react/lingua/src/components/mainpage/filter/tabs/AdvancedTab.js
+++ react/lingua/src/components/mainpage/filter/tabs/AdvancedTab.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import {Button, Checkbox, ControlLabel, FormControl, FormGroup} from "react-bootstrap";
+
+import './AdvanceTabs.css';
 
 class AdvancedTab extends Component {
     render() {
         return(
-            <p>Advanced</p>
+            <form className="padding__10">
+                <FormGroup controlId="keywordsFilterForm">
+                    <ControlLabel>Keywords</ControlLabel>
+                    <FormControl type="text" placeholder="Enter keywords" />
+                </FormGroup>
+                <FormGroup controlId="priceFilterForm">
+                    <ControlLabel>Price</ControlLabel>
+                    <div className="inline-form-container">
+                        <FormControl className="inline-form-member__2p5" type="text" placeholder="min" />
+                        <p className="inline-form-member__1">$ - </p>
+                        <FormControl className="inline-form-member__2p5" type="text" placeholder="max" />
+                        <p className="inline-form-member__1">$</p>
+                        <Checkbox className="inline-form-member__6">Free only</Checkbox>
+                    </div>
+                </FormGroup>
+                <div className="flex__justify-right">
+                    <Button type='submit'>Filter</Button>
+                </div>
+            </form>
         );
     }
 }
