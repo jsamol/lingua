a7f31aae7304715650a8294c3447b23e79ee78ab
jsamol <julia.samol@gmail.com>
2018-06-09 22:16:22 +0200

    Implement advanced filtering

--- react/lingua/src/containers/mainpage/courses/CoursesContainer.js
+++ react/lingua/src/containers/mainpage/courses/CoursesContainer.js
@@ -4,10 +4,60 @@ import {setSelectedCourseId} from "../../../actions/CoursesActions";
 
 import Courses from "../../../components/mainpage/courses/Courses";
 
-const getCourseWithImg = (state, filter) => {
+const checkKeywords = (keywords, course) => {
+    if (!keywords || keywords.length === 0) {
+        return true;
+    }
+
+    const keywordsSplit = keywords.toLowerCase()
+                                  .replace(",", " ")
+                                  .replace(":", " ")
+                                  .replace(";", " ")
+                                  .replace(/\s+/g, " ")
+                                  .split(" ");
+
+    let containsKeywords = true;
+
+    keywordsSplit.forEach(keyword => {
+        containsKeywords = containsKeywords
+            && (course.name.toLowerCase().includes(keyword)
+                || course.shortDescription.toLowerCase().includes(keyword)
+                || course.description.toLowerCase().includes(keyword));
+    });
+
+    return containsKeywords;
+};
+
+const checkMinAndMaxPrice = (minPrice, maxPrice, course) => {
+    const minPriceNum = parseFloat(minPrice);
+    const maxPriceNum = parseFloat(maxPrice);
+
+    const coursePrice = course.isFree ? "0" : course.price;
+    const coursePriceNum = parseFloat(coursePrice);
+
+    if (minPriceNum > -1 && maxPriceNum > -1) {
+        return minPriceNum <= coursePriceNum && coursePriceNum <= maxPriceNum;
+    } else if (minPriceNum > -1) {
+        return minPriceNum <= coursePriceNum;
+    } else if (maxPriceNum > -1) {
+        return coursePriceNum <= maxPriceNum;
+    }
+
+    return true;
+};
+
+const checkFreeOnly = (freeOnly, isCourseFree) => {
+    if (freeOnly) {
+        return isCourseFree;
+    }
+
+    return true;
+};
+
+const getFilteredCourseWithImg = (state, baseFilter) => {
     const languages = state.languages;
 
-    let filteredCourses = state.courses.filter(filter);
+    let filteredCourses = state.courses.filter(baseFilter);
 
     if (state.filterLanguages.length > 0) {
         filteredCourses = filteredCourses.filter(course => {
@@ -26,17 +76,18 @@ const getCourseWithImg = (state, filter) => {
 
 const mapStateToProps = state => {
     return ({
-        newCourses: getCourseWithImg(state, course => {
-            if (state.user &&
+        newCourses: getFilteredCourseWithImg(state, course => {
+            return state.user &&
                 !state.user.courses.includes(course.id) &&
-                state.user.languages.includes(course.languageId)) {
-                return course;
-            }
+                state.user.languages.includes(course.languageId) &&
+                checkKeywords(state.advancedFilter.keywords, course) &&
+                checkMinAndMaxPrice(state.advancedFilter.minPrice, state.advancedFilter.maxPrice, course) &&
+                checkFreeOnly(state.advancedFilter.freeOnly, course.isFree
+            )
         }),
-        openCourses: getCourseWithImg(state, course => {
-            if (state.user && state.user.courses.includes(course.id)) {
-                return course;
-            }
+        openCourses: getFilteredCourseWithImg(state, course => {
+            return state.user && state.user.courses.includes(course.id) &&
+                checkKeywords(state.advancedFilter.keywords, course)
         }),
         selectedCourseId: state.selectedCourseId
     });
