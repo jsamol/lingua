0e4d95631f2b9c7e21bdb507818e69f8b4f6ee62
jsamol <julia.samol@gmail.com>
2018-06-09 18:40:39 +0200

    Show scrollbar when the Filter panel overflows

--- react/lingua/src/components/mainpage/filter/Filter.js
+++ react/lingua/src/components/mainpage/filter/Filter.js
@@ -11,7 +11,7 @@ class Filter extends Component {
         return (
             <div className="height__100pc flex-col">
                 <h2 className="center-text">Filter</h2>
-                <Panel className="flex__1">
+                <Panel className="flex__1 overflow-y__auto">
                     <Panel.Body className="padding__0">
                         <Tabs defaultActiveKey={0} id="filter-tabs">
                             <Tab eventKey={0} title="Languages">
