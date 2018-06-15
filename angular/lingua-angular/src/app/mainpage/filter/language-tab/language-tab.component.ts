c5d0cc8d86a86b7893cd4b232423e4ef14f73038
jsamol <julia.samol@gmail.com>
2018-06-15 15:50:02 +0200

    List current languages

--- angular/lingua-angular/src/app/mainpage/filter/language-tab/language-tab.component.ts
+++ angular/lingua-angular/src/app/mainpage/filter/language-tab/language-tab.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Language} from "../../../../data/language";
+import {ApiService} from "../../../api.service";
 
 @Component({
   selector: 'app-language-tab',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./language-tab.component.css']
 })
 export class LanguageTabComponent implements OnInit {
+  languages: Language[] = [];
+  selectedLanguages: number[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.getCurrentLanguages();
   }
 
+  getCurrentLanguages() {
+    this.apiService.getUserData()
+      .subscribe(user => {
+        this.apiService.getLanguages()
+          .subscribe(languages => {
+            this.languages = languages.filter(language => {
+              return user.languages.includes(language.id);
+            });
+          });
+      });
+  }
+
+  toggleSelectedLanguage(languageId: number) {
+    if (this.selectedLanguages.includes(languageId)) {
+      const arrayId = this.selectedLanguages.indexOf(languageId);
+      this.selectedLanguages.splice(arrayId, 1);
+    } else {
+      this.selectedLanguages.push(languageId);
+    }
+  }
 }
