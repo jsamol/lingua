1fd88839ecfb8191ffe3a1f9f3ea42d8357b6b10
jsamol <julia.samol@gmail.com>
2018-06-15 15:20:34 +0200

    Add filter tabs

--- angular/lingua-angular/src/app/mainpage/filter/filter.component.ts
+++ angular/lingua-angular/src/app/mainpage/filter/filter.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  readonly LANGUAGE_TAB = "languages";
+  readonly ADVANCED_TAB = "advanced";
+
+  selectedTab = this.LANGUAGE_TAB;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setSelectedTab(selectedTab: string) {
+    this.selectedTab = selectedTab;
   }
 
 }
