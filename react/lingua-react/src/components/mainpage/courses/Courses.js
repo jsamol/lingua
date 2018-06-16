4329d649f01dc056574417ff0ad7c9f2ae754a00
jsamol <julia.samol@gmail.com>
2018-06-16 19:23:45 +0200

    Refactor Courses component

--- react/lingua-react/src/components/mainpage/courses/Courses.js
+++ react/lingua-react/src/components/mainpage/courses/Courses.js
@@ -7,45 +7,75 @@ import "./Courses.css";
 
 import Course from "../../../containers/mainpage/courses/CourseContainer";
 
-function NewCourses(props) {
-    const alwaysVisibleCourses = props.courses.slice(0, 3);
-    const hiddenCourses = props.courses.slice(3);
-    return(
-        <div>
-            {alwaysVisibleCourses.map(course => {
-                return(
-                    <Col key={course.id} sm={12} md={6} lg={4}>
-                        <Course id={course.id}
-                                name={course.name}
-                                img={course.img}
-                                desc={course.shortDescription}
-                                onClick={props.onClick}
-                                isFree={course.isFree}
-                                price={course.isFree ? "FREE" : "$" + course.price}
-                        />
-                    </Col>
-                );
-            })}
-            <Collapse in={!props.showLess}>
-                <div>
-                    {hiddenCourses.map(course => {
-                        return(
-                            <Col key={course.id} sm={12} md={6} lg={4}>
-                                <Course id={course.id}
-                                        name={course.name}
-                                        img={course.img}
-                                        desc={course.shortDescription}
-                                        onClick={props.onClick}
-                                        isFree={course.isFree}
-                                        price={course.isFree ? "FREE" : "$" + course.price}
-                                />
-                            </Col>
-                        );
-                    })}
-                </div>
-            </Collapse>
-        </div>
-    );
+class NewCourses extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            shouldShowLess: true
+        };
+
+        this.toggleShouldShowLess = this.toggleShouldShowLess.bind(this);
+    }
+
+    toggleShouldShowLess() {
+        this.setState(previousState => {
+            return {
+                shouldShowLess: !previousState.shouldShowLess
+            };
+        });
+    }
+
+    render() {
+        const alwaysVisibleCourses = this.props.courses.slice(0, 3);
+        const hiddenCourses = this.props.courses.slice(3);
+        return (
+            <div>
+                <Grid fluid>
+                    <Row>
+                        {alwaysVisibleCourses.map(course => {
+                            return (
+                                <Col key={course.id} sm={12} md={6} lg={4}>
+                                    <Course id={course.id}
+                                            name={course.name}
+                                            img={course.img}
+                                            desc={course.shortDescription}
+                                            onClick={this.props.onClick}
+                                            isFree={course.isFree}
+                                            price={course.isFree ? "FREE" : "$" + course.price}
+                                    />
+                                </Col>
+                            );
+                        })}
+                    </Row>
+                    <Collapse in={!this.state.shouldShowLess}>
+                        <Row>
+                            {hiddenCourses.map(course => {
+                                return (
+                                    <Col key={course.id} sm={12} md={6} lg={4}>
+                                        <Course id={course.id}
+                                                name={course.name}
+                                                img={course.img}
+                                                desc={course.shortDescription}
+                                                onClick={this.props.onClick}
+                                                isFree={course.isFree}
+                                                price={course.isFree ? "FREE" : "$" + course.price}
+                                        />
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    </Collapse>
+                </Grid>
+                {this.props.courses.length > 3 &&
+                <p onClick={this.toggleShouldShowLess}
+                   className="more-less-button">
+                    {this.state.shouldShowLess ? "More" : "Less"}
+                </p>
+                }
+            </div>
+        );
+    }
 }
 
 NewCourses.propTypes = {
@@ -58,24 +88,27 @@ NewCourses.propTypes = {
             isFree: PropTypes.bool.isRequired,
             price: PropTypes.string
         })
-    ).isRequired,
-    showLess: PropTypes.bool.isRequired
+    ).isRequired
 };
 
 function OpenCourses(props) {
     return(
-        props.courses.map(course => {
-            return(
-                <Col key={course.id} sm={12} md={6} lg={4}>
-                    <Course id={course.id}
-                            name={course.name}
-                            img={course.img}
-                            desc={course.shortDescription}
-                            onClick={props.onClick}
-                    />
-                </Col>
-            )
-        })
+        <Grid fluid>
+            <Row>
+                {props.courses.map(course => {
+                    return(
+                        <Col key={course.id} sm={12} md={6} lg={4}>
+                            <Course id={course.id}
+                                    name={course.name}
+                                    img={course.img}
+                                    desc={course.shortDescription}
+                                    onClick={props.onClick}
+                            />
+                        </Col>
+                    )
+                })}
+            </Row>
+        </Grid>
     );
 }
 
@@ -91,16 +124,6 @@ OpenCourses.propTypes = {
 };
 
 class Courses extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            shouldShowLessNewCourses: true
-        };
-
-        this.toggleShouldShowLessNewCourses = this.toggleShouldShowLessNewCourses.bind(this);
-    }
-
     static getDerivedStateFromProps(props, state) {
         if (!props.newCourses.map(course => { return course.id; }).includes(props.selectedCourseId)
             && !props.openCourses.map(course => { return course.id; }).includes(props.selectedCourseId)) {
@@ -108,14 +131,6 @@ class Courses extends Component {
         }
 
         return null;
-    }
-
-    toggleShouldShowLessNewCourses() {
-        this.setState(previousState => {
-            return {
-                shouldShowLessNewCourses: !previousState.shouldShowLessNewCourses
-            };
-        });
     }
 
     render() {
@@ -127,30 +142,9 @@ class Courses extends Component {
                 <Panel className="flex__1  overflow-y__auto">
                     <Panel.Body>
                         {areOpenCourses && <h3 className="margin__20">Start a new one...</h3>}
-
-                        <Grid fluid>
-                            <Row>
-                                <NewCourses courses={this.props.newCourses}
-                                            showLess={this.state.shouldShowLessNewCourses}
-                                />
-                            </Row>
-                        </Grid>
-
-                        {this.props.newCourses.length > 3 &&
-                            <p onClick={this.toggleShouldShowLessNewCourses}
-                               className="more-less-button">
-                                {this.state.shouldShowLessNewCourses ? "More" : "Less"}
-                            </p>
-                        }
-
+                        <NewCourses courses={this.props.newCourses}/>
                         {areOpenCourses && areNewCourses && <h3 className="margin__20">... or continue where you left off</h3>}
-
-                        <Grid fluid>
-                            <Row>
-                                <OpenCourses courses={this.props.openCourses}
-                                />
-                            </Row>
-                        </Grid>
+                        <OpenCourses courses={this.props.openCourses}/>
                     </Panel.Body>
                 </Panel>
             </div>
