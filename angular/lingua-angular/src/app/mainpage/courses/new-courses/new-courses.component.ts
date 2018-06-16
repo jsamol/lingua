13c99739fbaa49414896d0901d2529dd92ef8e0d
jsamol <julia.samol@gmail.com>
2018-06-16 13:59:38 +0200

    Hide and show more new courses

--- angular/lingua-angular/src/app/mainpage/courses/new-courses/new-courses.component.ts
+++ angular/lingua-angular/src/app/mainpage/courses/new-courses/new-courses.component.ts
@@ -7,11 +7,31 @@ import {Course} from "../../../../data/course";
   styleUrls: ['./new-courses.component.css']
 })
 export class NewCoursesComponent implements OnInit {
+  private readonly MORE_BUTTON_TEXT = "More";
+  private readonly LESS_BUTTON_TEXT = "Less";
+
   @Input() courses: Course[];
+  moreLessButtonText = this.MORE_BUTTON_TEXT;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getVisibleCourses(): Course[] {
+    return this.courses.slice(0, 3);
+  }
+
+  getHiddenCourses(): Course[] {
+    return this.courses.slice(3);
+  }
+
+  toggleMoreLess() {
+    if (this.moreLessButtonText === this.MORE_BUTTON_TEXT) {
+      this.moreLessButtonText = this.LESS_BUTTON_TEXT;
+    } else {
+      this.moreLessButtonText = this.MORE_BUTTON_TEXT;
+    }
   }
 
 }
